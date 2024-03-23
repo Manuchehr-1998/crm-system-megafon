d80ed8860ff972383fa3d33bf843825acd85e053
Manuchehr <manuchehrsaidov998@gmail.com>
2024-03-23 11:21:58 +0500

    start-project-commit

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar w-full h-full relative">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.png" width="100px" class="mx-auto" />
+    </NuxtLink>
+  </aside>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section>
+    <LayoutSidebar />
+  </section>
+</template>
