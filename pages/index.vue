351ac180c5b72d4f10e3b8fb1695117006ad4f5e
Manuchehr <manuchehrsaidov998@gmail.com>
2024-03-31 11:08:36 +0500

    full-pinia-commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ICard, IColumn } from "~/components/kanban/kanban.types";
+
+useHead({
+  title: "Home | CRM SYSTEM",
+});
+
+const dragCardRef = ref<ICard | null>(null);
+const sourseColumnRef = ref<IColumn | null>(null);
+</script>
 
 <template>
   <div class="p-10">
@@ -6,8 +15,8 @@
     <div>
       <UiCard>
         <UiCardHeader role="button">name Card</UiCardHeader>
-        <UiCardContent >Companiy</UiCardContent>
-        <UiCardFooter >Date</UiCardFooter>
+        <UiCardContent>Companiy</UiCardContent>
+        <UiCardFooter>Date</UiCardFooter>
       </UiCard>
     </div>
   </div>
